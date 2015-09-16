9d34c60d96a7d265ac91158a3d74c8415bac5364
MKS-students <mks.students@hackreactor.com>
2015-09-16 12:32:02 -0500

    Complete stack prototypal inheritance

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  instance.quantity = 0;
+
+  return instance;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  size: function () {
+    return this.quantity;
+  },
+  push: function (value) {
+    this.storage[this.quantity] = value;
+    this.quantity += 1;
+
+    return this.quantity;
+  },
+  pop: function () {
+    var temp;
+
+    if (this.quantity === 0) {
+      return undefined;
+    }
+    this.quantity -= 1;
+    temp = this.storage[this.quantity];
+    delete this.storage[this.quantity];
+
+    return temp;
+  }
+};
 
 
