03460e906cbd37621a9abbcaf4767c4148d44925
MKS-students <mks.students@hackreactor.com>
2015-09-16 11:52:23 -0500

    Completed Functional Pattern Stack & Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var firstIndex = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size += 1;
+    return size;
   };
 
   someInstance.dequeue = function(){
+    var temp;
+    if (someInstance.size() === 0) {
+      return undefined;
+    }
+    temp = storage[firstIndex];
+    delete storage[firstIndex];
+
+    firstIndex += 1;
+    return temp;
   };
 
   someInstance.size = function(){
+    return size - firstIndex;
   };
 
   return someInstance;
