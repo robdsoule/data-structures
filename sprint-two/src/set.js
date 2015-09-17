7e5e0fb6c492d2b348f8b54f0253b79229ef9d8c
MKS-students <mks.students@hackreactor.com>
2015-09-17 16:04:06 -0500

    Implemented Set Data Structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[JSON.stringify(item)] = item;
 };
 
 setPrototype.contains = function(item){
+  return this._storage[JSON.stringify(item)] !== undefined || false;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[JSON.stringify(item)];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ */
+
+/*
+    add()      : O(1), Directly accesses object using keys
+    contains() : O(1), Directly accesses object using keys
+    remove()   : O(1), Directly accesses object using keys
  */
