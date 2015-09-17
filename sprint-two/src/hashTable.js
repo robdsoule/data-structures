e6cf68bfa220fe8d283ea653c955fb6a5905aa83
MKS-students <mks.students@hackreactor.com>
2015-09-17 16:48:32 -0500

    Implement HashTable. Still need to handle collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,17 +3,42 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+// {
+//   hash : {
+//     key: {
+//       value
+//     }
+//   }
+
+// }
+
+
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var hash = getIndexBelowMaxForKey(k, this._limit);
+
+  // if (this._storage.get(hash) !== undefined && this._storage.get(hash) !== null) {
+  //   //
+  // }
+    this._storage.set(hash, v);
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var hash = getIndexBelowMaxForKey(k, this._limit);
 
+  return this._storage.get(hash);
+
+  // var existingHash = this._storage[hash];
+
+  // return this._storage[hash][JSON.stringify(k)];
 };
 
 HashTable.prototype.remove = function(k){
+  var hash = getIndexBelowMaxForKey(k, this._limit);
+  var temp = this._storage.get(hash)
 
+  this._storage.set(hash, null);
+
+  return temp;
 };
 
 
