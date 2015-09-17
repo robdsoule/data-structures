36050f4dae8378165b0828f3c3e8797fe225ab1c
MKS-students <mks.students@hackreactor.com>
2015-09-17 10:51:01 -0500

    Implemented LinkedList, addToTail(), removeHead(), contains()

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = new Node(value);
+    if (list.head === null) {
+      list.head = node;
+    } else if (list.tail !== null) {
+      list.tail.next = node;
+    }
+    list.tail = node;
+
   };
 
   list.removeHead = function(){
+    var temp;
+    if (list.head === null) {
+      return;
+    }
+    temp = list.head.value;
+    list.head = list.head.next;
+
+    return temp;
   };
 
   list.contains = function(target){
+    var contains = false;
+    var recurContains = function(node) {
+      if (node.value === target) {
+        contains = true;
+        return;
+      }
+      if (node.next) {
+        recurContains(node.next);
+      }
+    };
+
+    recurContains(list.head);
+    return contains;
   };
 
   return list;
@@ -27,3 +56,10 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+/*
+  addToTail()  : O(1) because we keep track of the tail node, immediately accessible
+  removeHead() : O(1) because we keep track of the head node, immediately accessible
+  contains()   : O(n) linearly depends on number of nodes in list, each node hit once until found
+ */
+
