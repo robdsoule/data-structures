1417040c675dad75f654dac8120c1f223d77ea4e
MKS-students <mks.students@hackreactor.com>
2015-09-19 16:27:29 -0500

    Completed breadthFirstLog() as well as tests for it

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -5,9 +5,10 @@ describe('binarySearchTree', function() {
     binarySearchTree = new BinarySearchTree(5);
   });
 
-  it('should have methods named "insert", "contains", and "depthFirstLog', function() {
+  it('should have methods named "insert", "contains", "breadthFirstLog" and "depthFirstLog"', function() {
     expect(binarySearchTree.insert).to.be.a("function");
     expect(binarySearchTree.contains).to.be.a("function");
+    expect(binarySearchTree.breadthFirstLog).to.be.a("function");
     expect(binarySearchTree.depthFirstLog).to.be.a("function");
   });
 
@@ -33,8 +34,55 @@ describe('binarySearchTree', function() {
     var func = function(value){ array.push(value); };
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
+    binarySearchTree.insert(4);
     binarySearchTree.insert(6);
+    binarySearchTree.insert(1);
     binarySearchTree.depthFirstLog(func);
-    expect(array).to.eql([5,2,3,6]);
+    expect(array).to.eql([5,2,1,3,4,6]);
   });
+
+  it('should execute a callback on every value in a tree using "breadthFirstLog"', function(){
+    var array = [];
+    var func = function(node) { array.push(node.value); };
+
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(1);
+    binarySearchTree.breadthFirstLog(func);
+    console.log(array);
+    expect(array).to.eql([5,2,6,1,3,4]);
+  });
+
+  it ('should keep track of the depth the node is', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    var depth3 = binarySearchTree.insert(4);
+    binarySearchTree.insert(6);
+    var depth2 = binarySearchTree.insert(1);
+    expect(depth3.depth).to.equal(3);
+    expect(depth2.depth).to.equal(2);
+  });
+
+  xit ('should traverse the tree and give a sorted array', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(1);
+    expect(binarySearchTree.traverseInOrder()).to.equal([1,2,3,4,5,6]);
+  });
+
+  //
+  // it ('should rebalance a tree when appropriate', function() {
+  //   binarySearchTree.insert(2);
+  //   binarySearchTree.insert(3);
+  //   binarySearchTree.insert(4);
+  //   binarySearchTree.insert(8);
+  //   binarySearchTree.insert(9);
+  //   binarySearchTree.insert(7);
+  //   binarySearchTree.insert(10);
+  //   binarySearchTree.insert(6);
+  // });
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,26 +1,29 @@
-var BinarySearchTree = function(value){
+var BinarySearchTree = function(value, depth){
   this.left = null;
   this.right = null;
+  this.depth = depth || 0;
+
   this.value = value;
 
 };
 
 BinarySearchTree.prototype.insert = function(value) {
-  var newTree = new BinarySearchTree(value);
+  var newTree = new BinarySearchTree(value, this.depth + 1);
 
   if (this.value > value) {
     if (this.left) {
-      this.left.insert(value);
+      return this.left.insert(value);
     } else {
       this.left = newTree;
     }
   } else {
     if (this.right) {
-      this.right.insert(value);
+      return this.right.insert(value);
     } else {
       this.right = newTree;
     }
   }
+  newTree.parent = this;
   return newTree;
 };
 
@@ -35,15 +38,11 @@ BinarySearchTree.prototype.contains = function(value) {
   } else {
     return (this.right ? this.right.contains(value) : false);
   }
-  // if (this.value > value) {
-  //   return this.left.contains(value);
-  // }
-  // return this.right.contains(value);
 
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(callback) {
-  callback(this.value);
+  callback(this.value, this);
   if (this.left) {
     this.left.depthFirstLog(callback);
   }
@@ -52,12 +51,47 @@ BinarySearchTree.prototype.depthFirstLog = function(callback) {
   }
 };
 
+BinarySearchTree.prototype.breadthFirstLog = function(callback, depth) {
+  depth = (depth + 1) || 0;
+  // var levelArray = [];
+  if (depth === 0) {
+    callback(this)
+  }
+
+  // Go through left and right in sync
+  if (this.left) {
+    callback(this.left);
+  }
+  if (this.right) {
+    callback(this.right);
+  }
+
+  // After, traverse in sync as well
+  if (this.left) {
+    this.left.breadthFirstLog(callback, depth);
+  }
+  if (this.right) {
+    this.right.breadthFirstLog(callback, depth);
+  }
+};
+
+// Autobalances the tree upon
+BinarySearchTree.prototype.autobalance = function() {
+
+};
+
+BinarySearchTree.prototype.rebalance = function() {
+
+};
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
 /*
-    insert()        : O(log(n)), rules out half of the values after each step, but could potentially be up to O(n) because it could be a one sided tree
-    contains()      : O(log(n)), rules out half of the values after each step, but could potentially be up to O(n) because it could be a one sided tree
-    depthFirstLog() : O(n), has to visit each node once
+    insert()          : O(log(n)), rules out half of the values after each step, but could potentially be up to O(n) because it could be a one sided tree
+    contains()        : O(log(n)), rules out half of the values after each step, but could potentially be up to O(n) because it could be a one sided tree
+    depthFirstLog()   : O(n), has to visit each node once
+    breadthFirstLog() : O(n), has to visit each node once
+    rebalance()       : O(n), has to visit each node at least once
  */
